71b0daa363c011130c6f43c59ad041f9fa77220b
jessmaranto <jessicamaranto@msn.com>
2024-10-01 21:11:19 -0600

    Add files via upload

---
+++ City_Markers/logic.js
@@ -0,0 +1,39 @@
+// Create a map object.
+let myMap = L.map("map", {
+  center: [37.09, -95.71],
+  zoom: 5
+});
+
+// Add a tile layer.
+L.tileLayer('https://{s}.tile.openstreetmap.org/{z}/{x}/{y}.png', {
+    attribution: '&copy; <a href="https://www.openstreetmap.org/copyright">OpenStreetMap</a> contributors'
+}).addTo(myMap);
+
+// An array containing each city's name, location, and population
+let cities = [{
+  location: [40.7128, -74.0059],
+  name: "New York",
+  population: 8550405
+},
+{
+  location: [41.8781, -87.6298],
+  name: "Chicago",
+  population: 2720546
+},
+{
+  location: [29.7604, -95.3698],
+  name: "Houston",
+  population: 2296224
+},
+{
+  location: [34.0522, -118.2437],
+  name: "Los Angeles",
+  population: 3971883
+},
+{
+  location: [41.2524, -95.9980],
+  name: "Omaha",
+  population: 446599
+}
+];
+
